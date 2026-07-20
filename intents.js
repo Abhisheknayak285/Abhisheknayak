2f27a5a569fe27893e36b2a96464323607196890
Abhishek Nayak <23amtics285@gmail.com>
2026-07-20 09:06:37 +0530

    Update intents.js

--- intents.js
+++ intents.js
@@ -1,1 +1,91 @@
+const INTENTS = {
 
+    greeting: [
+        "hi",
+        "hello",
+        "hey",
+        "hii",
+        "good morning",
+        "good afternoon",
+        "good evening"
+    ],
+
+    about: [
+        "about",
+        "introduce yourself",
+        "who are you",
+        "tell me about yourself",
+        "yourself",
+        "about abhishek",
+        "introduction"
+    ],
+
+    skills: [
+        "skills",
+        "technologies",
+        "tech stack",
+        "programming",
+        "languages",
+        "what can you do",
+        "html",
+        "css",
+        "javascript",
+        "firebase",
+        "react"
+    ],
+
+    projects: [
+        "project",
+        "projects",
+        "portfolio",
+        "work",
+        "applications",
+        "apps",
+        "website",
+        "antextshare",
+        "ar chats",
+        "encrypt"
+    ],
+
+    education: [
+        "education",
+        "college",
+        "degree",
+        "study",
+        "qualification",
+        "university",
+        "btech"
+    ],
+
+    contact: [
+        "contact",
+        "email",
+        "gmail",
+        "phone",
+        "location",
+        "address",
+        "hire",
+        "linkedin",
+        "github"
+    ],
+
+    experience: [
+        "experience",
+        "internship",
+        "career",
+        "job"
+    ],
+
+    thanks: [
+        "thanks",
+        "thank you",
+        "thx"
+    ],
+
+    goodbye: [
+        "bye",
+        "goodbye",
+        "see you"
+    ]
+
+};
